448d34792c205812d0e85005e21391fe05bf292e
karimkamaletdinov <450906k@gmail.com>
2025-01-25 19:37:54 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kmlkt/example-service
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port, portSpecified := os.LookupEnv("PORT")
+	if !portSpecified {
+		log.Printf("No port specified, so default will be used")
+		port = "80"
+	}
+	log.Printf("Listening on port %s", port)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
