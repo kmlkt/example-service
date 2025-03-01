551696c75ecad14bca43ae248336601bcba05c7c
Karim <65394332+kmlkt@users.noreply.github.com>
2025-03-02 00:19:12 +0500

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	}
 	log.Printf("Listening on port %s", port)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "You are at %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "DEV DEV DEV You are at %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
